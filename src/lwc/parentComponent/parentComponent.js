faacb89de190c3570d63e0a5863cd1ea144b09a0
skosgi1 <samsheeth.kosgi@gmail.com>
2020-11-09 11:27:59 -0800

    added custom label

--- src/lwc/parentComponent/parentComponent.js
+++ src/lwc/parentComponent/parentComponent.js
@@ -1,8 +1,9 @@
 import { LightningElement } from 'lwc';
+import parentObjectCustomLabel from '@salesforce/label/c.parentObjectCustomLabel';
 
 export default class ParentComponent extends LightningElement {
     parentLabel = 'The Drop Down name from Parent';
     parentPlaceHolder = 'The Place Holder From Parent';
-    parentObject = 'Account';
+    parentObject = parentObjectCustomLabel;
     parentField = 'type';
 }
